5522380390a1c87ec91ef03a376c2af9fe526771
DDumas551 <david.j.dumas@gmail.com>
2019-09-17 12:23:18 -0700

    Playing with react-bootstrap

---
+++ dissolvedsolids/src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="dSLogo">Dissolved Solids</p>
+      <Row>
+        {/* <Col>
+          <Card className="cardd" style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://d2ees1g486p7cy.cloudfront.net/media/mastheads/original_5uLqFsh7Rg30OyeBZ4n5FY19_SUMMER2B_WIFI-LANDING_NITRO-TRIO_Masthead-Desktop---1800x560_FINAL070419.jpg?v=63731299474"
+            />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card className="cardd" style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://d2ees1g486p7cy.cloudfront.net/media/mastheads/original_5uLqFsh7Rg30OyeBZ4n5FY19_SUMMER2B_WIFI-LANDING_NITRO-TRIO_Masthead-Desktop---1800x560_FINAL070419.jpg?v=63731299474"
+            />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col> */}
+      </Row>
+    </div>
+  );
+}
+
+export default App;
